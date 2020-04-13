2bcafe5d2f6014bbe64b9facf696a4b331856ce2
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-04-13 16:03:36 +0300

    feat: add resolve task Calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,120 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    var getArguments = function(expression, sign) {
+        var argumentsSet = [];
+        var argument = '';
+        var bracketsCount = 0;
+          
+        for (var i = 0; i < expression.length; i++) {
+            var char = expression[i];
+            switch (char) {
+                case '(':
+                    bracketsCount++;
+                    break;
+                case ')':
+                    bracketsCount--;
+                    break;
+                case sign:
+                    if (!bracketsCount) {
+                        argumentsSet.push(argument);
+                        argument = '';
+                        char = '';
+                    }
+                    break;
+            }
+            argument += char;
+        }
+
+        if (bracketsCouner) {
+                throw new Error('ExpressionError: Brackets must be paired');
+            }
+            argumentsSet.push(argument);
+            return argumentsSet;
+    }
+
+    var add = function(expression) {
+        var result = 0;
+        var argumentsArr = getArguments(expression, '+');
+
+        argumentsArr = argumentsArr.map( 
+            function(item) {
+                return subtract(item);
+            }
+        );
+
+        result = argumentsArr.reduce( 
+            function(previousValue, currentValue) {
+                return +previousValue + (+currentValue);
+            }
+        );
+
+        return result;
+    }
+
+    var subtract = function(expression) {
+        var result = 0;
+        var argumentsArr = getArguments(expression, '-');
+        
+        argumentsArr = argumentsArr.map( 
+            function(item) {
+                return multiply(item);
+            }
+        );
+
+        result = argumentsArr.reduce( 
+            function(previousValue, currentValue) {
+                return previousValue - currentValue;
+            }
+        );
+        return result;
+    }
+
+    var multiply = function(expression) {
+        var result = 0;
+        var argumentsArr = getArguments(expression, '*');
+
+        argumentsArr = argumentsArr.map( 
+            function(item) {
+                return divide(item);
+            }
+        );
+
+        result = argumentsArr.reduce( 
+            function(previousValue, currentValue) {
+                return previousValue * currentValue;
+            }
+        );
+        return result;
+    }
+
+    var divide = function(expression) {
+        var result = 0;
+        var argumentsArr = getArguments(expression, '/');
+
+        argumentsArr = argumentsArr.map( 
+            function(item) {
+                item = item.trim();
+                if ( item[0] === "(" && item[item.length - 1] === ")" ) {
+                    return add(item.slice(1, -1));
+                }
+                return item;
+            }
+        );
+                
+        result = argumentsArr.reduce( 
+            function(previousValue, currentValue) {
+                if (currentValue === '0') {
+                    throw new Error('TypeError: Division by zero.');
+                }
+                return previousValue / currentValue;
+            }
+        );
+        return result;
+    }
+    
+    return add(expr);
 }
 
 module.exports = {
